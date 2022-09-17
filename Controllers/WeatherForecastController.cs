ad4bc104972f90569863fffaad56182b9d7e242e
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2022-09-17 11:51:57 -0300

    Adicionei páginas de login

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,32 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
+using static System.IO.File;
 
 namespace csharp_backend.Controllers;
 
 [ApiController]
-[Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+[Route("/")]
+public class TestController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
+    [HttpGet]
+    public ActionResult Index()
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
+        return Content(
+            ReadAllText("Views/index.html"), 
+            "text/html");
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    [HttpGet("/login")]
+    public ActionResult Login()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        return Content(
+            ReadAllText("Views/login.html"), 
+            "text/html");
     }
 }
